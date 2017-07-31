f96ae817d783d7817e20578618c6e737bd7616c7
Eimantas <eimantas.stonys@gmail.com>
2017-07-31 21:07:15 +0200

    Added initial game API views

--- oak/oak/urls.py
+++ oak/oak/urls.py
@@ -6,6 +6,14 @@ from oak import views as api_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^actions/set-action$', api_views.SetActionView.as_view(), name='set_action'),
-
+    url(
+        r'^game/init$',
+        api_views.GameInitializationView.as_view(),
+        name='init_game'
+    ),
+    url(
+        r'^game/set-action$',
+        api_views.GameActionView.as_view(),
+        name='set_game_acion'
+    ),
 ]
--- oak/oak/views.py
+++ oak/oak/views.py
@@ -1,12 +1,26 @@
-from rest_framework.generics import RetrieveUpdateAPIView
-from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+class GameInitializationView(APIView):
+    '''An API view to create a new Game object when a new game is initialised
+    via frontend
+    '''
+    permission_classes = (
+    )
 
-class SetActionView(APIView):
+    http_method_names = ['get', ]
+
+    def get(self, request, format=None):
+
+        resp = {'a': 'b'}
+
+        return Response(resp)
+
+
+class GameActionView(APIView):
+    '''An API view to log game action
+    '''
     permission_classes = (
     )
 
