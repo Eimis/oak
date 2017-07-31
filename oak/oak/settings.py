3e184130846f9c5b13d0b1949b7c7bfea8a8d6db
Eimantas <eimantas.stonys@gmail.com>
2017-07-31 21:28:35 +0200

    Installed app that adds CORS headers to APi responses

--- oak/oak/settings.py
+++ oak/oak/settings.py
@@ -34,6 +34,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '127.0.0.1:3000',
+)
+
 
 # Application definition
 
@@ -46,10 +51,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'oak',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
