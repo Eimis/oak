61fe4afe7a01ad7a32a5a00ce49348f1637fcb1b
Eimantas <eimantas.stonys@gmail.com>
2017-08-01 15:28:09 +0200

    Proper field names for GameLog model

--- oak/oak/models.py
+++ oak/oak/models.py
@@ -16,8 +16,8 @@ class GameLog(models.Model):
     )
 
     # FIXME: use rows and columns?:
-    from_field = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
-    to_field = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
+    from_column = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
+    to_column = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
     player = models.CharField(max_length=1, choices=GAME_PLAYER_CHOICES)
     added_on = models.DateTimeField(auto_now_add=True)
     game = models.ForeignKey(Game)
