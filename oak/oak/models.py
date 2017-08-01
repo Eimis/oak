031633133e13b59a44f86413e2e67f93936456ed
Eimantas <eimantas.stonys@gmail.com>
2017-08-01 15:31:02 +0200

    Proper choices for GameLog column fields

--- oak/oak/models.py
+++ oak/oak/models.py
@@ -10,14 +10,15 @@ class GameLog(models.Model):
         ('X', 'X'),
         ('O', 'O'),
     )
-    GAME_FIELD_CHOICES = (
-        ('X', 'X'),
-        ('O', 'O'),
+    GAME_COLUMN_CHOICES = (
+        ('0', '0'),
+        ('1', '1'),
+        ('2', '2'),
     )
 
     # FIXME: use rows and columns?:
-    from_column = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
-    to_column = models.CharField(max_length=1, choices=GAME_FIELD_CHOICES)
+    from_column = models.CharField(max_length=1, choices=GAME_COLUMN_CHOICES)
+    to_column = models.CharField(max_length=1, choices=GAME_COLUMN_CHOICES)
     player = models.CharField(max_length=1, choices=GAME_PLAYER_CHOICES)
     added_on = models.DateTimeField(auto_now_add=True)
     game = models.ForeignKey(Game)
