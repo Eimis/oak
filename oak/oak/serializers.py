eb87e55f93630ddcfaa87097695ec85383168c90
Eimantas <eimantas.stonys@gmail.com>
2017-08-02 23:54:22 +0200

    Use different date format for serializing GameLog object
    (for easier testing)

--- oak/oak/serializers.py
+++ oak/oak/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from oak import models as oak_models  # circular dependency
 
 
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
 class GameSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -12,7 +15,20 @@ class GameSerializer(serializers.ModelSerializer):
 
 
 class GameLogSerializer(serializers.ModelSerializer):
+    # This field is explicitly defined because we want to use different
+    # date format and defining this setting globally probably isn't a very good
+    # idea. Also, it'seasier to test this field.
+    added_on_date = serializers.DateTimeField(
+        source='added_on', format=DATE_FORMAT
+    )
 
     class Meta:
         model = oak_models.GameLog
-        fields = ('__all__')
+        fields = (
+            'game',
+            'id',
+            'player',
+            'to_column',
+            'to_row',
+            'added_on_date',
+        )
