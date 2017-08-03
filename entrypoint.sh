94a6eeec5e4cfcb546d2fe8b8af3480071de0a40
Eimantas <eimantas.stonys@gmail.com>
2017-08-03 07:49:05 +0200

    Wait before running django migrations on container build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#sometimes postgresql server is started too quickly so automatic build fails:
+sleep 4
+
 python3 /code/oak/manage.py makemigrations
 python3 /code/oak/manage.py migrate
 exec "$@"
